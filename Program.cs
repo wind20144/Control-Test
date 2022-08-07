3d302d726f818a94cff205823974a2608a46f206
wind <wind20144@gmail.com>
2022-08-07 16:39:29 +0300

    код программы

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+string[] array = { "hello", "2", "world", ":-)" };
+
+methodArray(array);
+
+void methodArray(string[] array)
+{
+    string[] newarr = new string[array.Length];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+
+        if (array[i].Length <= 3)
+        {
+            newarr[i] = array[i];
+
+            Console.Write($"[{String.Join(",", newarr[i])}]");
+        }
+
+    }
+
+}
+
